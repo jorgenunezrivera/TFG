7f9954613dc0edadb18272182d64a717defa60f4
Jorge <jorgenunezrivera@github.com>
2021-02-22 18:45:11 +0100

    1200 steps lr 0.00001 intermediate rewards

--- codigo/it1/deep_q_learning.py
+++ codigo/it1/deep_q_learning.py
@@ -267,11 +267,11 @@ def deep_q_learning(env,
             # Take a step
             action_probs = policy(state, epsilon)            
             action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
-            #if (i_episode+1) % 20 == 0:#mostrar las acciones cada 20 samples
-            #    print("action =" + str(action)+ " epsilon = " + str(epsilons[min(total_t, epsilon_decay_steps-1)]))
+            if (i_episode+1) % 20 == 0:#mostrar las acciones cada 20 samples
+                print("action =" + str(action)+ " epsilon = " + str(epsilons[min(total_t, epsilon_decay_steps-1)]))
             next_state, reward, done, _ = env.step(action)
-            #if (i_episode+1) % 20 == 0 and reward!=0:#mostrar las acciones cada 20 samples
-            #    print("reward =" + str(reward))
+            if (i_episode+1) % 20 == 0 and reward!=0:#mostrar las acciones cada 20 samples
+                print("reward =" + str(reward))
              
 
             # If our replay memory is full, pop the first element
