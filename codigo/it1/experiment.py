c2718ecf5cd8871f1385f1febda8193e1248214e
Jorge <jorgenunezrivera@github.com>
2021-03-14 23:34:56 +0100

    memory leaks

--- codigo/it1/experiment.py
+++ codigo/it1/experiment.py
@@ -4,3 +4,7 @@ print("experiment 1. Deep Q learning step_size 32, max_Steps 18")
 name=deep_q_learning_train(num_episodes=12000,learning_rate=0.00001,update_target_freq=120,validate_freq=4000,max_steps=18,
                                                                                     step_size=32,intermediate_rewards=0,continue_until_dies=0)
 print("result: {}".format(name))
+print("experiment 2. Deep Q learning step_size 32, max_Steps 1 continue_until_dies 1")
+name=deep_q_learning_train(num_episodes=12000,learning_rate=0.00001,update_target_freq=120,validate_freq=4000,max_steps=1,
+                                                                                    step_size=32,intermediate_rewards=0,continue_until_dies=1)
+print("result: {}".format(name))
