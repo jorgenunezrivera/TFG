cd3e4a09d56c7de21eabd8f4a00c2a628f1936c9
Jorge <jorgenunezrivera@github.com>
2021-04-06 00:08:21 +0200

    reinforce fixed
    reinfrorce experiment
    
    pretrained_mobilenet

--- codigo/it1/experiment.py
+++ codigo/it1/experiment.py
@@ -12,9 +12,10 @@ VALIDATION_IMAGES_DIR = "validation1000"
 VALIDATION_LABELS_FILE = "validation_labels.txt"
 env = ImageWindowEnvGenerator(VALIDATION_IMAGES_DIR, VALIDATION_LABELS_FILE,10,32,2,1,3,1)
 random_reward,random_hits=random_env_test(env)
+print("Random test. reward: {} hits: {}".format(random_reward,random_hits))
 check_dataset_posibilities(env,5)
 
-print("Random test. reward: {} hits: {}".format(random_reward,random_hits))
+
 
 
 print("experiment 1. Reinforce step_size 32, max_Steps 6  continue_until_dies 1 intermediate rewards 2")
