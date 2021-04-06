24fe65cbd030f4f79b78df93af2032f1928e2e2b
Jorge <jorgenunezrivera@github.com>
2021-04-06 12:13:02 +0200

    reinforce fixed
    reinfrorce experiment
    
    pretrained_mobilenet

--- codigo/it1/experiment.py
+++ codigo/it1/experiment.py
@@ -10,17 +10,17 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 
 
 
-print("experiment 1. Reinforce step_size 32, max_Steps 6  continue_until_dies 1 intermediate rewards 2")
-name=reinforce_train(num_episodes=12000,learning_rate=0.00001,validate_freq=4000,max_steps=6,
-                                                                                    step_size=32,intermediate_rewards=2,continue_until_dies=1)
-print("result: {}".format(name))
+#print("experiment 1. Reinforce step_size 32, max_Steps 6  continue_until_dies 1 intermediate rewards 2")
+#name=reinforce_train(num_episodes=12000,learning_rate=0.00001,validate_freq=4000,max_steps=6,
+#                                                                                    step_size=32,intermediate_rewards=2,continue_until_dies=1)
+#print("result: {}".format(name))
 #
 
 #
-print("experiment 2. Reinforce step_size 32, max_Steps 10  continue_until_dies 0 intermediate rewards 2")
-name=reinforce_train(num_episodes=12000,learning_rate=0.00001,validate_freq=4000,max_steps=10,
-                                                                                    step_size=32,intermediate_rewards=2,continue_until_dies=0,n_actions=3)
-print("result: {}".format(name))
+#print("experiment 2. Reinforce step_size 32, max_Steps 10  continue_until_dies 0 intermediate rewards 2")
+#name=reinforce_train(num_episodes=12000,learning_rate=0.00001,validate_freq=4000,max_steps=10,
+#                                                                                    step_size=32,intermediate_rewards=2,continue_until_dies=0,n_actions=3)
+#print("result: {}".format(name))
 
 
 print("experiment 3. Deep Q learning step_size 32, max_Steps 10  continue_until_dies 0 intermediate rewards 2 model=mobienet")
