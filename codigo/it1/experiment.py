3b7a46ee3482400a825a4591fa86c2c18d9e8f2e
Jorge <jorgenunezrivera@github.com>
2021-05-23 22:53:20 +0200

    Fixed Stats Tests for different models

--- codigo/it1/experiment.py
+++ codigo/it1/experiment.py
@@ -8,6 +8,11 @@ from reinforce import reinforce_train
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 
+print("experiment 0. Fast reinforce atari")
+name=reinforce_train(num_episodes=2000,learning_rate=0.0000001,validate_freq=4000,max_steps=6,
+                                                                                    step_size=32,continue_until_dies=1,model_name="atari")
+print("result: {}".format(name))
+
 print("experiment 1. DQN  atari")
 name=deep_q_learning_train(num_episodes=2000,learning_rate=0.00001,validate_freq=1000,max_steps=6,
                                                                                     step_size=32,continue_until_dies=1,model_name="atari")
