65a3ebefd71ccdc3f59db2ce86b2382de5b3d357
Jorge <jorgenunezrivera@github.com>
2021-05-26 21:39:31 +0200

    LAST REINFORCE EXPERIMENT

--- codigo/it1/experiment.py
+++ codigo/it1/experiment.py
@@ -8,35 +8,35 @@ from reinforce import reinforce_train
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 
-print("experiment 0. Fast reinforce atari")
-name=reinforce_train(num_episodes=2000,learning_rate=0.0000001,validate_freq=1000,max_steps=6,
-                                                                                    step_size=32,continue_until_dies=1,model_name="atari")
-print("result: {}".format(name))
+#print("experiment 0. Fast reinforce atari")
+#name=reinforce_train(num_episodes=2000,learning_rate=0.0000001,validate_freq=1000,max_steps=6,
+#                                                                                    step_size=32,continue_until_dies=1,model_name="atari")
+#print("result: {}".format(name))
 
-print("experiment 1. DQN  atari")
-name=deep_q_learning_train(num_episodes=2000,learning_rate=0.00001,validate_freq=1000,max_steps=6,
-                                                                                    step_size=32,continue_until_dies=1,model_name="atari")
-print("result: {}".format(name))
+#print("experiment 1. DQN  atari")
+#name=deep_q_learning_train(num_episodes=2000,learning_rate=0.00001,validate_freq=1000,max_steps=6,
+#                                                                                    step_size=32,continue_until_dies=1,model_name="atari")
+#print("result: {}".format(name))
 
-print("experiment 2. DQN  alex")
-name=deep_q_learning_train(num_episodes=2000,learning_rate=0.00001,validate_freq=1000,max_steps=6,
-                                                                                    step_size=32,continue_until_dies=1,model_name="alexnet")
-print("result: {}".format(name))
+#print("experiment 2. DQN  alex")
+#name=deep_q_learning_train(num_episodes=2000,learning_rate=0.00001,validate_freq=1000,max_steps=6,
+#                                                                                    step_size=32,continue_until_dies=1,model_name="alexnet")
+#print("result: {}".format(name))
 
-print("experiment 3. DQN  mobilenet")
-name=deep_q_learning_train(num_episodes=2000,learning_rate=0.00001,validate_freq=1000,max_steps=6,
-                                                                                    step_size=32,continue_until_dies=1,model_name="mobilenet")
-print("result: {}".format(name))
+#print("experiment 3. DQN  mobilenet")
+#name=deep_q_learning_train(num_episodes=2000,learning_rate=0.00001,validate_freq=1000,max_steps=6,
+#                                                                                    step_size=32,continue_until_dies=1,model_name="mobilenet")
+#print("result: {}".format(name))
 
-print("experiment 4. Reinforce atari")
-name=reinforce_train(num_episodes=12000,learning_rate=0.0000001,validate_freq=4000,max_steps=6,
-                                                                                    step_size=32,continue_until_dies=1,model_name="atari")
-print("result: {}".format(name))
+#print("experiment 4. Reinforce atari")
+#name=reinforce_train(num_episodes=12000,learning_rate=0.0000001,validate_freq=4000,max_steps=6,
+#                                                                                    step_size=32,continue_until_dies=1,model_name="atari")
+#print("result: {}".format(name))
 
-print("experiment 5. Reinforce alex")
-name=reinforce_train(num_episodes=12000,learning_rate=0.0000001,validate_freq=4000,max_steps=6,
-                                                                                    step_size=32,continue_until_dies=1,model_name="alexnet")
-print("result: {}".format(name))
+#print("experiment 5. Reinforce alex")
+#name=reinforce_train(num_episodes=12000,learning_rate=0.0000001,validate_freq=4000,max_steps=6,
+#                                                                                    step_size=32,continue_until_dies=1,model_name="alexnet")
+#print("result: {}".format(name))
 
 print("experiment 6. Reinforce mobilenet")
 name=reinforce_train(num_episodes=12000,learning_rate=0.0000001,validate_freq=4000,max_steps=6,
