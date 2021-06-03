0853e4f1a5cd8f4af93b5a3e7cc979cd66072a53
Jorge <jorgenunezrivera@github.com>
2021-06-04 00:23:07 +0200

    top5 final plot results deep_q_learning_test with top5 env tests

--- codigo/it1/tests.py
+++ codigo/it1/tests.py
@@ -9,13 +9,14 @@ VALIDATION_IMAGES_DIR = "validation1000"
 TRAINING_LABELS_FILE = "training_labels.txt"
 VALIDATION_LABELS_FILE = "validation_labels.txt"
 
-env=ImageWindowEnv(VALIDATION_IMAGES_DIR, VALIDATION_LABELS_FILE, 6, 32,continue_until_dies=1, is_validation=1)
+env=ImageWindowEnv(VALIDATION_IMAGES_DIR, VALIDATION_LABELS_FILE, 10, 32,continue_until_dies=1, is_validation=1)
 
 
 check_dataset_posibilities(env, 5)
+
 
 print("max_steps:{} step_size:{} continue_until_dies:{} n_actions:{} ".format
           (env.max_steps,env.step_size,env.continue_until_dies,env.n_actions))
 random_env_test_batch(env,10)
 
-#
+random_env_test_batch(env,10)
